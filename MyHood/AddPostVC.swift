246cfffb1b42893a364e5c001dcfe8f74c3eec40
Sachin Virdy <svirdy1@gmail.com>
2015-11-08 15:12:04 -0800

    add image picker

--- MyHood/AddPostVC.swift
+++ MyHood/AddPostVC.swift
@@ -8,29 +8,41 @@
 
 import UIKit
 
-class AddPostVC: UIViewController {
+class AddPostVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var postImage: UIImageView!
     @IBOutlet weak var titleField: UITextField!
     @IBOutlet weak var descriptionField: UITextField!
     
+    var imagePicker: UIImagePickerController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         postImage.layer.cornerRadius = postImage.frame.size.width / 2
         postImage.clipsToBounds = true
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
     }
 
     
     @IBAction func addPicButtonPressed(sender: UIButton!) {
         sender.setTitle("", forState: .Normal)
+        presentViewController(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func makePostButtonPressed(sender: AnyObject) {
+        if let title = titleField.text, let desc = descriptionField.text, let img = postImage.image {
+            
+        }
     }
 
     @IBAction func cancelButtonPressed(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        postImage.image = image
     }
 }
