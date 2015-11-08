ef6a78fc25b5d9a18e399515bc041490b27fd681
Sachin Virdy <svirdy1@gmail.com>
2015-11-08 15:13:40 -0800

    add data service singleton

---
+++ MyHood/DataService.swift
@@ -0,0 +1,13 @@
+//
+//  DataService.swift
+//  MyHood
+//
+//  Created by Sachin Virdy on 11/8/15.
+//  Copyright © 2015 Sachin Virdy. All rights reserved.
+//
+
+import Foundation
+
+class DataService {
+    static let instance = DataService()
+}
