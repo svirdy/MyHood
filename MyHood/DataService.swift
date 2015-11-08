ee67d7074a4a91ab3214978e8d21d4177bd93745
Sachin Virdy <svirdy1@gmail.com>
2015-11-08 15:16:41 -0800

    create initial functions for saving and loading posts and images

--- MyHood/DataService.swift
+++ MyHood/DataService.swift
@@ -7,7 +7,36 @@
 //
 
 import Foundation
+import UIKit
 
 class DataService {
     static let instance = DataService()
+    
+    private var _loadedPosts = [Post]()
+    
+    var loadedPosts: [Post] {
+        return _loadedPosts
+    }
+    
+    func savePosts() {
+        
+    }
+    
+    func loadPosts() {
+        
+    }
+    
+    func saveImageAndCreatePath(image: UIImage) {
+        
+    }
+    
+    func imageForPath(path: String) {
+        
+    }
+    
+    func addPost(post: Post) {
+        _loadedPosts.append(post)
+        savePosts()
+        loadPosts()
+    }
 }
